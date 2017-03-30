cd6143cb88aefd2df02985c7aa230b7de8315690
stgilbert <stgilbert@5087H52.gsu.edu>
2017-03-30 10:27:44 -0400

    redfish bluefish

--- src/edu/gsu/csc1302/prj2/somethingrandom/SpadesPlayer.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/SpadesPlayer.java
@@ -1,4 +1,7 @@
 package edu.gsu.csc1302.prj2.somethingrandom;
+
+import edu.gsu.csc1302.coll1.Deck;
+
 /**
  * Spades Player to manage the four AI player types.
  *
@@ -6,8 +9,20 @@ package edu.gsu.csc1302.prj2.somethingrandom;
  *
  */
 public class SpadesPlayer {
+	/**
+	 * Int to track.
+	 */
+	private int playerType;
+
+	/**
+	 * Player constructor.
+	 * @param type int
+	 * @param hand Deck
+	 */
+	public SpadesPlayer(final int type, final Deck hand) {
 
 
 
+	}
 
 }
