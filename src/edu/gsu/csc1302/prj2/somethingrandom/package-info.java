867a7f4c69637e5bf2b9f3931b9f4cbdc685b21c
J-Dierberger-Laptop <quantum.di@bellsouth.net>
2017-03-30 09:39:06 -0400

    Packages

---
+++ src/edu/gsu/csc1302/prj2/somethingrandom/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Something Random team's project package.
+ * We will be creating players for the Spades card game.
+ * @author J-Dierberger
+ */
+package edu.gsu.csc1302.prj2.somethingrandom;
---
+++ src/edu/gsu/csc1302/prj2/somethingrandom/testing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Testing package for the project.
+ * @author J-Dierberger
+ */
+package edu.gsu.csc1302.prj2.somethingrandom.testing;
