a71c1487de90a7cc5358d6441b56038a2c31d6d1
jakep <jakep@DESKTOP-9NC7FHO.gsu.edu>
2017-03-30 10:00:46 -0400

    BLAH BLeah Bloop

---
+++ src/edu/gsu/csc1302/prj2/somethingrandom/DrSeuss.java
@@ -0,0 +1,13 @@
+package edu.gsu.csc1302.prj2.somethingrandom;
+
+/**
+ * This is a joke.
+ * @author jakep
+ * Trying to see if these things work!
+ */
+public class DrSeuss {
+	/**
+	 * I just hate it when checkstyle yells at me.
+	 */
+	int fish = 1;
+}
