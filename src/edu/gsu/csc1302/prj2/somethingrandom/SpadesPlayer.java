ece0e9bf11438b6463ffb89062e69eaf8f38414d
J-Dierberger-Laptop <quantum.di@bellsouth.net>
2017-03-30 15:26:53 -0400

    Player updates

--- src/edu/gsu/csc1302/prj2/somethingrandom/SpadesPlayer.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/SpadesPlayer.java
@@ -7,18 +7,44 @@ import edu.gsu.csc1302.coll1.Deck;
  * Spades Player to manage the four AI player types.
  *
  * @author stgilbert
+ * @author J-Dierberger
  *
  */
 public class SpadesPlayer implements PlayerInterface {
 
 	/**
-	 * Int to track.
+	 * Aggressive player type constant.
+	 */
+	public static final int TYPE_AGGRESSIVE = 0;
+
+	/**
+	 * Chicken player type constant.
+	 */
+	public static final int TYPE_CHICKEN = 1;
+
+	/**
+	 * Wildcard player type constant.
+	 */
+	public static final int TYPE_WILDCARD = 2;
+
+	/**
+	 * Savagely intelligent player type constant.
+	 */
+	public static final int TYPE_INTELLIGENT = 3;
+
+	/**
+	 * User controlled player constant.
+	 */
+	public static final int TYPE_USER = 4;
+
+	/**
+	 * Int to track the actual designated player type.
 	 */
 	private int playerType;
 
 	/**
 	 * Player constructor.
-	 * @param type int
+	 * @param type Player type to use.
 	 * @param controller Controller object for the game
 	 */
 	public SpadesPlayer(final int type, final Controller controller) {
