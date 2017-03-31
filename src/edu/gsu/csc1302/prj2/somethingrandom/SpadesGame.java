8dbb8e9e0512228e895cc72b6a966b3a505b9fbc
J-Dierberger-Laptop <quantum.di@bellsouth.net>
2017-03-31 10:43:19 -0400

    Cleaned up class

--- src/edu/gsu/csc1302/prj2/somethingrandom/SpadesGame.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/SpadesGame.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import edu.gsu.csc1302.coll1.Card;
 import edu.gsu.csc1302.coll1.Card.Rank;
 import edu.gsu.csc1302.coll1.Card.Suit;
-import edu.gsu.csc1302.prj2.somethingrandom.SpadesPlayer;
 
 /**
  * Spades Player to manage the generic game stuff.
@@ -14,73 +13,83 @@ import edu.gsu.csc1302.prj2.somethingrandom.SpadesPlayer;
  * @author Wesley
  *
  */
-public class SpadesGame {
-	public static void main(String[] args) {
+public final class SpadesGame {
 
+	/**
+	 * Fixing checkstyle errors.
+	 */
+	private SpadesGame() { }
 
-	Scanner in = new Scanner(System.in);
-	LinkedList<Card> fullDeck = new LinkedList<>();
-	LinkedList<Card> shuffledDeck = new LinkedList<>();
-	LinkedList<Card> deck1 = new LinkedList<>();
-	LinkedList<Card> deck2 = new LinkedList<>();
-	LinkedList<Card> deck3 = new LinkedList<>();
-	LinkedList<Card> deck4 = new LinkedList<>();
-	Controller controller = new Controller();
+	/**
+	 * Now we don't have to reinstantiate the scanner every time we need it.
+	 */
+	public static final Scanner IN = new Scanner(System.in);
 
+	/**
+	 * Main methpd.
+	 * @param args Console launch args.
+	 */
+	public static void main(final String[] args) {
 
-	for (Suit s : Suit.values()) {
-		for (Rank r : Rank.values()) {
-			Card adder = new Card(s, r);
-			fullDeck.add(adder);
+		LinkedList<Card> fullDeck = new LinkedList<>();
+		LinkedList<Card> shuffledDeck = new LinkedList<>();
+		LinkedList<Card> deck1 = new LinkedList<>();
+		LinkedList<Card> deck2 = new LinkedList<>();
+		LinkedList<Card> deck3 = new LinkedList<>();
+		LinkedList<Card> deck4 = new LinkedList<>();
+		Controller controller = new Controller();
 
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				Card adder = new Card(s, r);
+				fullDeck.add(adder);
+
+			}
 		}
-	}
 
-	for (int shuffleNum = 52; shuffleNum != 0; shuffleNum--) {
-		int cardNum = (int) (Math.random() * shuffleNum);
-		shuffledDeck.add(fullDeck.get(cardNum));
-		fullDeck.remove(cardNum);
-	}
+		for (int shuffleNum = 52; shuffleNum != 0; shuffleNum--) {
+			int cardNum = (int) (Math.random() * shuffleNum);
+			shuffledDeck.add(fullDeck.get(cardNum));
+			fullDeck.remove(cardNum);
+		}
 
-	for (int deal = 0; deal < 13; deal++) {
-		deck1.add(shuffledDeck.get(0));
-		shuffledDeck.remove(0);
-		deck2.add(shuffledDeck.get(0));
-		shuffledDeck.remove(0);
-		deck3.add(shuffledDeck.get(0));
-		shuffledDeck.remove(0);
-		deck4.add(shuffledDeck.get(0));
-		shuffledDeck.remove(0);
-	}
+		for (int deal = 0; deal < 13; deal++) {
+			deck1.add(shuffledDeck.get(0));
+			shuffledDeck.remove(0);
+			deck2.add(shuffledDeck.get(0));
+			shuffledDeck.remove(0);
+			deck3.add(shuffledDeck.get(0));
+			shuffledDeck.remove(0);
+			deck4.add(shuffledDeck.get(0));
+			shuffledDeck.remove(0);
+		}
 
+		//	System.out.println("What play style would you like player Four to be?");
+		SpadesPlayer playerOne = new SpadesPlayer(1, controller);
+		//	System.out.println("What play style would you like player Two to be??");
+		SpadesPlayer playerTwo = new SpadesPlayer(1, controller);
+		// System.out.println("What play style would you like player Three to be?");
+		SpadesPlayer playerThree = new SpadesPlayer(1, controller);
+		//	System.out.println("What play style would you like player Four to be?");
+		SpadesPlayer playerFour = new SpadesPlayer(1, controller);
+		DeckImplementation deck11 = new DeckImplementation(deck1);
+		DeckImplementation deck22 = new DeckImplementation(deck2);
+		DeckImplementation deck33 = new DeckImplementation(deck3);
+		DeckImplementation deck44 = new DeckImplementation(deck4);
 
+		playerOne.setHand(deck11);
+		playerTwo.setHand(deck22);
+		playerThree.setHand(deck33);
+		playerFour.setHand(deck44);
 
-//	System.out.println("What play style would you like player Four to be?");
-	SpadesPlayer playerOne = new SpadesPlayer(1, controller);
-//	System.out.println("What play style would you like player Two to be??");
-	SpadesPlayer playerTwo = new SpadesPlayer(1, controller);
-//	System.out.println("What play style would you like player Three to be?");
-	SpadesPlayer playerThree = new SpadesPlayer(1, controller);
-//	System.out.println("What play style would you like player Four to be?");
-	SpadesPlayer playerFour = new SpadesPlayer(1, controller);
-	DeckImplementation deck11 = new DeckImplementation(deck1);
-	DeckImplementation deck22 = new DeckImplementation(deck2);
-	DeckImplementation deck33 = new DeckImplementation(deck3);
-	DeckImplementation deck44 = new DeckImplementation(deck4);
+		//	System.out.println(fullDeck);
+		//	System.out.println(shuffledDeck);
+		System.out.println(deck11);
+		System.out.println(deck22);
+		System.out.println(deck33);
+		System.out.println(deck44);
 
-	playerOne.setHand(deck11);
-	playerTwo.setHand(deck22);
-	playerThree.setHand(deck33);
-	playerFour.setHand(deck44);
-
-//	System.out.println(fullDeck);
-//	System.out.println(shuffledDeck);
-	System.out.println(deck11);
-	System.out.println(deck22);
-	System.out.println(deck33);
-	System.out.println(deck44);
-
-	in.close();
+		IN.close();
 	}
 }
 
