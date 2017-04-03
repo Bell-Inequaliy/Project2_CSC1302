b30df37a6a6e1bd92bac5f8e6aa84f25e8d6a7b0
jakep <jakep@10.0.0.233>
2017-04-02 20:36:46 -0400

    Controller Basics
    I've added values and methods to the controller class. 
    I'm probably either missing something or getting something wrong, but
    this is at least a baseline to work from.

--- src/edu/gsu/csc1302/prj2/somethingrandom/Controller.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/Controller.java
@@ -3,14 +3,54 @@ package edu.gsu.csc1302.prj2.somethingrandom;
 import java.util.List;
 
 import edu.gsu.csc1302.coll1.Card;
+import edu.gsu.csc1302.coll1.Deck;
 
 /**
  * Controller class. Handles each individual game's stuffs.
  * @author J-Dierberger
  */
 public class Controller {
+	/**
+	 * The cards that are on the table.
+	 */
+	private Deck table = new DeckImplementation();
+	/**
+	 * The score for this round.
+	 */
+	private int roundScore;
+	/**
+	 * The Dealer's team's bid.
+	 */
+	private int dealerBid;
+	/**
+	 * The dealer's opponent's team's bid.
+	 */
+	private int rivalBid;
+	/**
+	 * This is where the players are kept.
+	 * Let's hope they don't figure out how to get out.
+	 */
+	private List<SpadesPlayer> players;
+	/**
+	 * The player whose turn it currently is.
+	 */
+	private SpadesPlayer currentPlayer;
 
-	// some array of players
+	/**
+	 * Constructs a Controller with input players.
+	 * @param player1 is the dealer.
+	 * @param player2 is the second player.
+	 * @param player3 is the third player, player1's teammate.
+	 * @param player4 is the fourth player, player2's teammate.
+	 */
+	public Controller(final SpadesPlayer player1, final SpadesPlayer player2,
+			final SpadesPlayer player3, final SpadesPlayer player4) {
+		players.add(player1);
+		players.add(player2);
+		players.add(player3);
+		players.add(player4);
+		currentPlayer = player1;
+	}
 
 	/**
 	 * Get the given player's team mate from the controller's array of players.
@@ -19,7 +59,15 @@ public class Controller {
 	 */
 	public SpadesPlayer getTeamMate(final SpadesPlayer player) {
 		// TODO Make this work.
-		return null;
+		if (players.contains(player)) {
+			if (players.indexOf(player) < 3) {
+				return players.get(players.indexOf(players) + 2);
+			} else {
+				return players.get(players.indexOf(players) - 2);
+			}
+		} else {
+			return null;
+		}
 	}
 
 	/**
@@ -27,7 +75,61 @@ public class Controller {
 	 * @return The List of cards that have been played, ordered from P1 to P4.
 	 */
 	public List<Card> getPlayedCards() {
-		return null;
+		return table;
 	}
-
+	/**
+	 * plays a new card at the next spot.
+	 * @param card The card being played.
+	 */
+	public void playCard(final Card card) {
+		table.add(card);
+	}
+	/**
+	 * This passes the turn to the next player.
+	 */
+	public void nextTurn() {
+		currentPlayer = players.get(players.indexOf(currentPlayer) + 1);
+	}
+	/**
+	 * Sets the bid for the dealer's team.
+	 * @param bid the amount being bid
+	 */
+	public void setDealerBid(final int bid) {
+		dealerBid = bid;
+	}
+	/**
+	 * Sets the bid for the rival's team.
+	 * @param bid the amount being bid
+	 */
+	public void setRivalBid(final int bid) {
+		rivalBid = bid;
+	}
+	/**
+	 * Returns the dealer team's bid.
+	 * @return the aforementioned bid
+	 */
+	public int getDealerBid() {
+		return dealerBid;
+	}
+	/**
+	 * Returns the dealer team's bid.
+	 * @return the aforementioned bid
+	 */
+	public int getRivalBid() {
+		return rivalBid;
+	}
+	/**
+	 * Sets the round's score.
+	 * @param score the value of the score being set
+	 */
+	public void setScore(final int score) {
+		roundScore = score;
+	}
+	/**
+	 * This method returns the round's score.
+	 * @return Take a wild guess.
+	 */
+	public int getScore() {
+		return roundScore;
+	}
 }
