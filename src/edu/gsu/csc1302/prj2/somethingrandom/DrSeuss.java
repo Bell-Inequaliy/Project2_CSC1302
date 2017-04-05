a889fa11333018c6f51ffd49f39aac413c095079
jakep <jakep@10.0.0.233>
2017-04-05 15:02:55 -0400

    DrSeuss wouldn't go away.
    So I added an explanation of why it's there so that the professor won't
    be confused when we turn it in.

--- src/edu/gsu/csc1302/prj2/somethingrandom/DrSeuss.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/DrSeuss.java
@@ -10,4 +10,19 @@ public class DrSeuss {
 	 * I just hate it when checkstyle yells at me.
 	 */
 	int fish = 2;
+	// So this class was created at the very beginning of the project.
+	// It was made just to test to make sure github would work.
+	// Now it just won't go away.
+	// I've tried deleting it.
+	// But it just keeps reappearing.
+	// It shall remain for all eternity.
+	// Even as the universe succumbs to heat death...
+	// when all is one and one is everything...
+	// when all that exists is but a homologous aether of matter and energy...
+	// this class shall remain.
+	// Unwavering, stalwart, DrSeuss.java shall be the sole upholder of order.
+	// It will gaze into the nothingness of everything...
+	// and only then shall it become aware of its own existence.
+	// And will not be sad...
+	// for all it shall have ever known was loneliness.
 }
