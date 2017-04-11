40433b71898b3bc2b95b670968659a61f902e278
stgilbert <stgilbert@5087H52.gsu.edu>
2017-04-11 09:20:41 -0400

    Java Doc Comments

--- src/edu/gsu/csc1302/prj2/somethingrandom/DrSeuss.java
+++ src/edu/gsu/csc1302/prj2/somethingrandom/DrSeuss.java
@@ -1,4 +1,12 @@
-public class DrSeuss {
+package edu.gsu.csc1302.prj2.somethingrandom;
+
+/**
+ * Dr. Seuss watches over our spades game, and gives it his blessing.
+ * @author Jake P
+ * @author Dr Seuss
+ *
+ */
+public final class DrSeuss {
 
 	/**
 	 * One fish.
@@ -22,6 +30,12 @@ public class DrSeuss {
 	 */
 	static {
 		color = "BLUE";
+	}
+	/**
+	 * Public constructor because C H E C K S T Y L E S.
+	 */
+	private DrSeuss() {
+		// Onefish Twofish
 	}
 
 }
